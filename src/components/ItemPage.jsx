9b392cb0b9bcab9df49c83d8faed5b53e5bf2a23
oscartan2233@gmail.com <oscartan2233@gmail.com>
2024-11-13 16:47:40 +0800

    Update

--- src/components/ItemPage.jsx
+++ src/components/ItemPage.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useStock } from '../stockContext';
+import { FaFilter } from 'react-icons/fa';
+import '../assets/css/ItemPage.css';
 
 const ItemPage = ({ }) => {
     const location = useLocation();
     const data = location.state;
+    const { stocks } = useStock();
     
     useEffect(() => {
         if (data) {
@@ -14,7 +18,28 @@ const ItemPage = ({ }) => {
     }, [])
 
     return (
-        <div>1 item</div>
+        <div className="item-container">
+            <h1 className="w-100 mb-4 text-center font-custom">Category Name</h1>
+            <div className="item-filter-container">
+                <div className="item-filter-button">
+                    <button className="">
+                        Show Filters
+                    </button>
+                </div>
+            </div>
+            <div className="item-boxes-container">
+                <div className="item-box-container col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-3">
+                    <div className="item-box-image">
+                        
+                    </div>
+                    <h5 className="font-custom">999.9/24K BUNGA RAYA GOLD BAR(100G)</h5>
+                    <p className="font-custom">Price: RM1000 ~ RM2000</p>
+                    <p className="font-custom">Weight: 1 ~ 2</p>
+                    <p className="font-custom">Width: 13 ~ 29</p>
+                    <p className="font-custom">Gold Type: Silver</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
