f4366781acf26b930ed9a631aee46bda5e8ead8b
Oscartan <131570694+oscartan2223@users.noreply.github.com>
2024-11-15 16:59:45 -0800

    update

--- src/components/ItemPage.jsx
+++ src/components/ItemPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useStock } from '../stockContext';
-import { FaFilter } from 'react-icons/fa';
+import { FaFilter, FaTimes } from 'react-icons/fa';
 import '../assets/css/ItemPage.css';
 
 const ItemPage = () => {
@@ -10,7 +10,17 @@ const ItemPage = () => {
     const { stocks } = useStock();
     const [stockList, setStockList] = useState();
     const [currentItemList, setCurrentItemList] = useState([]);
-    
+    const [filterBox, setFilterBox] = useState(false);
+
+    useEffect(() => {
+        if (filterBox) {
+            document.documentElement.style.overflow = 'hidden';
+        } else {
+            document.documentElement.style.overflow = 'auto';
+        }
+    }, [filterBox]);
+
+
     useEffect(() => {
         if (data) {
             console.log(data);  // Log the data if it's passed via navigate
@@ -21,23 +31,57 @@ const ItemPage = () => {
 
     useEffect(() => {
         if (stocks.current) {
-            const items = stocks.current.flatMap(eachGroup => 
+            const items = stocks.current.flatMap(eachGroup =>
                 eachGroup.items.map(eachItem => eachItem)
             );
             setCurrentItemList(items);
         }
     }, [stocks]);
-    
+
 
     return (
         <div className="item-container">
             <h1 className="w-100 mb-4 text-center font-custom">Category Name</h1>
             <div className="item-filter-container">
                 <div className="item-filter">
-                    <span className="item-filter-button">
+                    <span className="item-filter-button" onClick={() => { setFilterBox(!filterBox) }}>
                         <FaFilter className="item-filter-icon" />
                         <label>Show Filters</label>
                     </span>
+                    <div className={`item-filter-box ${filterBox ? 'item-filter-box-show' : 'item-filter-box-hide'}`}>
+                        <div className="item-filter-title font-custom fw-bold mb-4">
+                            FILTERS
+                            <button className="item-filter-close-btn" onClick={() => { setFilterBox(!filterBox) }}>
+                                <FaTimes />
+                            </button>
+                        </div>
+                        <div className="overflow-auto">
+                            <div className="mb-3">
+                                <button className="w-100 font-custom-2 fw-bold text-start p-0">
+                                    Types
+                                </button>
+                                <div className={`d-flex flex-column`}>
+                                    <span className="w-100 font-custom-2">All</span>
+                                    <span className="w-100 font-custom-2">Gold</span>
+                                    <span className="w-100 font-custom-2">Silver</span>
+                                </div>
+                            </div>
+
+                            <div>
+                                <button className="w-100 font-custom-2 fw-bold text-start p-0">
+                                    Advanced
+                                </button>
+                                <div className={`d-flex flex-column`}>
+                                    <span className="w-100 font-custom-2">With box only</span>
+                                    <span className="w-100 font-custom-2">Certificate only</span>
+                                </div>
+                            </div>
+
+                                Price, Measurement and Weight.....
+                            </div>
+                    </div>
+                    <div className={`item-filter-box-overlay ${filterBox ? 'item-filter-box-overlay-show' : ''}`} onClick={() => { setFilterBox(!filterBox) }}>
+                    </div>
                 </div>
             </div>
             <div className="item-boxes-container d-flex flex-wrap">
